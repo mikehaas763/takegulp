0f1ef9a4363bf5f12ad7c7e797b1b8613928c13c
Mike Haas <mike@emken.co>
2015-10-18 11:54:56 -0400

    Run typescript filter second

--- tasks/buildFactory.js
+++ tasks/buildFactory.js
@@ -12,9 +12,7 @@ function buildFactory(config, gulp) {
 
 
         return gulp.src(config.sourceTree, {base: './'})
-            .pipe(tsFilter)
-            .pipe(typeScript(tsProject))
-            .pipe(tsFilter.restore)
+            .pipe(jsFilter)
             .pipe(babel(config.babelOptions))
             .on('error', function(e) {
                 console.log('>>> ERROR', e);
@@ -22,6 +20,11 @@ function buildFactory(config, gulp) {
                 this.emit('end');
             })
             .pipe(jsFilter.restore)
+            // tsfilter must come after js filter because the output of tsfilter is .js files which will get matched
+            // by the jsfilter. Another option is to use two separate gulp streams and merge them after.
+            .pipe(tsFilter)
+            .pipe(typeScript(tsProject))
+            .pipe(tsFilter.restore)
             .pipe(gulp.dest(config.buildOutputLocation));
     };
 
